34f233e756a61d784ba57e3033b90a965c761621
boben195 <boben195@ukr.net>
2025-07-15 12:58:31 +0300

    add coffee js

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Navbar from "./components/Navbar/Navbar";
 import theme from "./assets/theme.js";
 import { ThemeProvider } from "@mui/material";
 import Hero from "./components/Hero/Hero.jsx";
+import Coffee from "./components/Coffee/Coffee.jsx";
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Navbar />
         <Hero />
+        <Coffee />
       </ThemeProvider>
     </>
   );
