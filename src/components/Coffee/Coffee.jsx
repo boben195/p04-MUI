cf05d2acf1e1cfef6c0dc59fa8359971a3cde50c
boben195 <boben195@ukr.net>
2025-07-15 14:40:09 +0300

    fin

--- src/components/Coffee/Coffee.jsx
+++ src/components/Coffee/Coffee.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import products from "../../assets/coffee.js";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Button,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+} from "@mui/material";
+import Grid from "@mui/material/Grid";
 
 const Coffee = () => {
-  return <div>Coffee</div>;
+  return (
+    <Box sx={{ my: "5rem" }} id="coffee">
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        Featured Coffees
+      </Typography>
+      <Grid container spacing={3} sx={{ padding: 3, justifyContent: "center" }}>
+        {products.map((product) => (
+          <Grid item="true" xs={12} sm={6} md={4}>
+            <Card sx={{ maxWidth: 345, textAlign: "center", padding: 2 }}>
+              <CardMedia
+                component="img"
+                height="200"
+                image={product.image}
+                alt={product.title}
+              />
+              <CardContent>
+                <Typography variant="h6">{product.title}</Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {product.description}
+                </Typography>
+                <Typography variant="h6" color="primary" sx={{ marginY: 1 }}>
+                  ${product.price.toFixed(2)}
+                </Typography>
+                <Button variant="contained" color="secondary">
+                  Buy Now
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Coffee;
